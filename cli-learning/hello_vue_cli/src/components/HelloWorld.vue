ecae9f494a65f1bd9be4fab300ac5895fc5e331f
linj2n <linj2n@163.com>
2019-04-09 22:54:07 +0800

    🚧 Init cli-demo
    NONE #31

---
+++ cli-learning/hello_vue_cli/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p class="msg"> {{ msg }} </p>
+    </div>
+</template>
+<script>
+// 向外暴露一个默认对象
+export default {
+    data () {
+        return {
+            msg: 'Hello Vue.js'
+        }
+    }   
+}
+</script>
+<style>
+    .msg {
+        color: blue;
+        font-size: 30px;
+    }
+</style>
+
+
